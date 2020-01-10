0d4a8b2a11a54ec2f4937604bc4cc81bb7ecd8e1
jjuriz <jjuriz@gmail.com>
2020-01-10 13:58:13 -0500

    Completed initial challenge.

--- jskids.js
+++ jskids.js
@@ -5,9 +5,9 @@
 // Children under 13 and unaccompanied by an adult may not see PG-13 films.
 // Code is needed to return a BOOLEAN value whether the child may see the movie. 
 
-//if age < 12 && accompanied by adult = FALSE then return FALSE
-//if age < 12 && accompanied by adult = TRUE then return TRUE
-//if age >= 13 && unaccompanied by an adult return TRUE
+//if age < 12 && accompanied by adult = FALSE
+//if age < 12 && accompanied by adult = TRUE
+//if age >= 13 return TRUE (adult status unnecessary)
 //Ternary operator may address the 12 and under...
 
 let childAge = document.getElementById("child_age"),
@@ -15,18 +15,21 @@ let childAge = document.getElementById("child_age"),
     checkButton = document.getElementById("check_button"),
     displayAnswer = document.getElementById("display_answer");
 
+let checkChildAge = function checkChildAge(){
+        if (childAge.value >= 13 || adultPresent.value.toLowerCase() == "yes")
+        return true;
+        else 
+        return false;
+    }
 
-let ageCheck = function ageCheck(){
-    if (childAge.value < 13 && adultPresent.value)
-    return true;
-    else if (childAge.value >= 13 || adultPresent.value)
-    return true;
-    else 
-    return false;
-};
+checkButton.addEventListener("click", function(){
+    displayAnswer.innerHTML += checkChildAge() + "<br>";
+});
 
-
-    checkButton.addEventListener("click", function(){
-        displayAnswer.innerHTML += ageCheck() + "<br>";
-    });
-
+// What I struggled with:
+// 1. I understood the logic of what was needed quickly. However after a day 
+// I simplified the necessary logic down to one OR SVGPatternElement. 
+// 2. Didn't know how to get BOOLEAN value from an HTML checkbox. Googled but the 
+// available answers were too complicated for me at this point. Next I tried radio buttons
+// then dropdowns, all to no avail. Finally went with the worst option of textbox. This worked.
+// 3. Though I completed the challenge it highlighed just how little I know. 
